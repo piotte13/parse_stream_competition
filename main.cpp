ec2fcd97e0f8762f4579da757481ae4249385c5f
piotte13 <jeremie.piotte@gmail.com>
2019-11-13 18:07:00 -0500

    new jsonstream threaded version

--- main.cpp
+++ main.cpp
@@ -7,23 +7,49 @@
 #include "libraries/rapidjson/document.h"
 #include "libraries/rapidjson/writer.h"
 #include "libraries/rapidjson/stringbuffer.h"
+#include "libraries/rapidjson/error/en.h"
 
 #include "libraries/mmapping/MemoryMapped.h"
 #include "libraries/mmapping/MemoryMapped.cpp"
+
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define BLUE "\x1B[34m"
+#define MAGENTA "\x1B[35m"
+#define END_COLOR "\033[0m"
+
+int nb_iterations = 10;
+bool allow_mmap = true;
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
         std::cerr << "Please specify a filename " << std::endl;
     }
     const char * filename = argv[1];
-    //simdjson::padded_string p = simdjson::get_corpus(filename);
-    MemoryMapped data(filename, MemoryMapped::WholeFile);
-    const char* raw = (const char*) data.getData();
-    const size_t len = data.size();
+    const char* raw;
+    size_t len;
+    std::string data_string;
+    simdjson::padded_string p;
+    auto data = MemoryMapped{};
+    if(allow_mmap){
+        data.open(filename, MemoryMapped::WholeFile);
+        raw = (const char*) data.getData();
+        len = data.size();
+        data_string = std::string(raw,len);
+    }
+    else{
+        auto p1 = simdjson::get_corpus(filename);
+        p.swap(p1);
+        raw = p.data();
+        len = p.size();
+        data_string = std::string(raw,len);
+    };
 
-    std::wclog << "simdjson: Getline + normal parse... " << std::endl;
+
+
+    std::cout << GREEN  << "simdjson: Getline + normal parse... " << END_COLOR << std::endl;
     std::cout << "Gigabytes/second\t" << "Nb of documents parsed" << std::endl;
-    for (auto i = 0; i < 1; i++) {
+    for (auto i = 0; i < nb_iterations; i++) {
         //Actual test
         simdjson::ParsedJson pj;
         bool allocok = pj.allocate_capacity(len);
@@ -31,13 +57,13 @@ int main(int argc, char *argv[]) {
             std::cerr << "failed to allocate memory" << std::endl;
             return EXIT_FAILURE;
         }
-        std::istringstream ss(std::string(raw,len));
+        std::istringstream ss(data_string);
 
         auto start = std::chrono::steady_clock::now();
         int count = 0;
         std::string line;
         int parse_res = simdjson::SUCCESS;
-        while (getline(ss, line)) {
+        while (getline(ss, line) && parse_res == simdjson::SUCCESS) {
             parse_res = simdjson::json_parse(line, pj);
             count++;
         }
@@ -49,45 +75,49 @@ int main(int argc, char *argv[]) {
         std::cout << std::setprecision(3) << speedinGBs << "\t\t\t\t" << count << std::endl;
 
         if (parse_res != simdjson::SUCCESS) {
-            std::cerr << "Parsing failed" << std::endl;
+            std::wcerr << "Parsing failed with: " << simdjson::error_message(parse_res).c_str() << std::endl;
             exit(1);
         }
     }
 
 
 
-    std::wclog << "rapidjson: Getline + normal parse... " << std::endl;
+//    std::cout << MAGENTA << "rapidjson: Getline + normal parse... " << END_COLOR << std::endl;
+//    std::cout << "Gigabytes/second\t" << "Nb of documents parsed" << std::endl;
+//    for (auto i = 0; i < nb_iterations; i++) {
+//        //Actual test
+//        std::istringstream ss(data_string);
+//        rapidjson::Document d;
+//
+//        auto start = std::chrono::steady_clock::now();
+//        int count = 0;
+//        std::string line;
+//        while (getline(ss, line) && !d.HasParseError()) {
+//            d.Parse(line.data());
+//            count++;
+//        }
+//
+//        auto end = std::chrono::steady_clock::now();
+//        d.Clear();
+//        std::chrono::duration<double> secs = end - start;
+//        double speedinGBs = (len) / (secs.count() * 1000000000.0);
+//        std::cout << std::setprecision(3) << speedinGBs << "\t\t\t\t" << count << std::endl;
+//
+//        if (d.HasParseError()) {
+//            std::wcerr << "Parsing failed with: " <<  GetParseError_En(d.GetParseError()) << std::endl;
+//            exit(1);
+//        }
+//    }
+
+
+
+
+    std::cout << BLUE << "simdjson: jsonstream" << END_COLOR << std::endl;
     std::cout << "Gigabytes/second\t" << "Nb of documents parsed" << std::endl;
-    for (auto i = 0; i < 1; i++) {
-        //Actual test
-        std::istringstream ss(std::string(raw, len));
-        rapidjson::Document d;
-
-        auto start = std::chrono::steady_clock::now();
-        int count = 0;
-        std::string line;
-        int parse_res = simdjson::SUCCESS;
-        while (getline(ss, line)) {
-            d.Parse(line.data());
-            count++;
-        }
-
-        auto end = std::chrono::steady_clock::now();
-
-        std::chrono::duration<double> secs = end - start;
-        double speedinGBs = (len) / (secs.count() * 1000000000.0);
-        std::cout << std::setprecision(3) << speedinGBs << "\t\t\t\t" << count << std::endl;
-    }
-
-
-
-
-    std::wclog << "simdjson: jsonstream \n" << std::endl;
-    std::cout << "Gigabytes/second\t" << "Nb of documents parsed" << std::endl;
-    for (auto i = 0; i < 1; i++) {
+    for (auto i = 0; i < nb_iterations; i++) {
         //Actual test
         simdjson::ParsedJson pj;
-        simdjson::JsonStream js{raw, len, 1000000};
+        simdjson::JsonStream js{raw, len, 300000};
         int parse_res = simdjson::SUCCESS_AND_HAS_MORE;
 
         auto start = std::chrono::steady_clock::now();
@@ -106,7 +136,7 @@ int main(int argc, char *argv[]) {
         std::cout << std::setprecision(3) << speedinGBs << "\t\t\t\t" << count << std::endl;
 
         if (parse_res != simdjson::SUCCESS) {
-            std::cerr << "Parsing failed" << std::endl;
+            std::wcerr << "Parsing failed with: " << simdjson::error_message(parse_res).c_str() << std::endl;
             exit(1);
         }
     }
